cf6c7a3300defa337565984fdd33d1e3679399a6
Lindsey <lindsey@creens.com>
2025-07-08 15:52:15 -0500

    fixed content areas

--- elements/Quote_Small_Headshot/element.php
+++ elements/Quote_Small_Headshot/element.php
@@ -140,13 +140,13 @@ class Quotesmallheadshot extends \Breakdance\Elements\Element
         false,
         [],
       ), c(
-        "image",
-        "Image",
+        "subhead",
+        "Subhead",
         [c(
-        "image",
-        "Image",
+        "text",
+        "Text",
         [],
-        ['type' => 'wpmedia', 'layout' => 'vertical'],
+        ['type' => 'text', 'layout' => 'vertical', 'textOptions' => ['multiline' => true]],
         false,
         false,
         [],
@@ -156,8 +156,34 @@ class Quotesmallheadshot extends \Breakdance\Elements\Element
         false,
         [],
       ), c(
-        "content",
-        "Content",
+        "buttons",
+        "Buttons",
+        [c(
+        "remove_",
+        "Remove?",
+        [],
+        ['type' => 'toggle', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), getPresetSection(
+      "EssentialElements\\AtomV1ButtonContent",
+      "Primary Button",
+      "primary_button",
+       ['condition' => [[['path' => 'content.buttons.remove_', 'operand' => 'is not set', 'value' => '']]], 'type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\AtomV1ButtonContent",
+      "Secondary Button",
+      "secondary_button",
+       ['condition' => [[['path' => 'content.buttons.remove_', 'operand' => 'is not set', 'value' => '']]], 'type' => 'popout']
+     )],
+        ['type' => 'section', 'layout' => 'vertical'],
+        false,
+        false,
+        [],
+      ), c(
+        "quotes",
+        "Quotes",
         [c(
         "slides",
         "Slides",
