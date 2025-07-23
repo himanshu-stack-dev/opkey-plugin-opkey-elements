a40ccf83765747829d28e9ecfd3c8b32d05ddf91
Lindsey <lindsey@creens.com>
2025-07-23 16:35:12 -0500

    rounded out cta edits

--- elements/CTA_Simple/element.php
+++ elements/CTA_Simple/element.php
@@ -4,7 +4,6 @@ namespace BreakdanceCustomElements;
 
 use function Breakdance\Elements\c;
 use function Breakdance\Elements\PresetSections\getPresetSection;
-use function OpkeyCustomElements\getSharedDefaults;
 
 
 \Breakdance\ElementStudio\registerElementForEditing(
@@ -71,9 +70,7 @@ class CTAsimple extends \Breakdance\Elements\Element
 
     static function defaultProperties()
     {
-        return [
-            'content' => getSharedDefaults(),
-        ];
+        return ['content' => ['eyebrow' => ['text' => 'Lorem ipsum dolor'], 'heading' => ['text' => 'Lorem ipsum dolor sit amet'], 'subhead' => ['text' => 'Lorem ipsum dolor sit amet consectetur adipiscing elit'], 'content' => ['text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.'], 'buttons' => ['primary_button' => ['text' => 'Contact sales', 'link' => '#'], 'secondary_button' => ['text' => 'Learn more', 'link' => '#']]]];
     }
 
     static function defaultChildren()
@@ -138,6 +135,14 @@ class CTAsimple extends \Breakdance\Elements\Element
         "eyebrow",
         "Eyebrow",
         [c(
+        "",
+        "",
+        [],
+        ['type' => 'alert_box', 'layout' => 'vertical', 'alertBoxOptions' => ['style' => 'default', 'content' => '<p>Only visible on mobile</p>']],
+        false,
+        false,
+        [],
+      ), c(
         "text",
         "Text",
         [],
@@ -169,15 +174,7 @@ class CTAsimple extends \Breakdance\Elements\Element
       ), c(
         "buttons",
         "Buttons",
-        [c(
-        "remove_",
-        "Remove?",
-        [],
-        ['type' => 'toggle', 'layout' => 'inline'],
-        false,
-        false,
-        [],
-      ), getPresetSection(
+        [getPresetSection(
       "EssentialElements\\AtomV1ButtonContent",
       "Primary Button",
       "primary_button",
